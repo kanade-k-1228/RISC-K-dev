47c375128c81fa688881bb7c957180d734292db5
kanade-k-1228 <kanade.k.1228@gmail.com>
2022-11-28 17:47:46 +0900

    Timeslice Scheduler

--- rkrtos/01_timeslice/run.sh
+++ rkrtos/01_timeslice/run.sh
@@ -1,5 +1,6 @@
 cat ../lib.rk main.rk > a.rk
 rkasm a.rk
-rkemu -d main.dump -i main.intr a.rk.bin
+# rkemu -d main.dump -i main.intr a.rk.bin
 # rkemu a.rk.bin
-# rkemu -i main.intr a.rk.bin
+# rkemu -to -d main.dump a.rk.bin
+rkemu -i main.intr a.rk.bin
