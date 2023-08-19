aacaa6bdf4c38556e9f7b7780620abb369976ab1
kanade-k-1228 <kanade.k.1228@gmail.com>
2023-08-19 20:00:11 +0900

    fix hex

---
+++ rk16/verilog/fixhex.py
@@ -0,0 +1,3 @@
+with open("rom.inv.hex") as f:
+    for l in f.readlines():
+        print(l[6:8],l[4:6],l[2:4],l[0:2],sep='')
--- rk16/verilog/rom/rom.sv
+++ rk16/verilog/rom/rom.sv
@@ -5,7 +5,7 @@ module rom (
 
   reg [31:0] memory[0:64*1024-1];
   initial begin
-    $readmemh("./rom.hex", memory);
+    $readmemh("build/rom.hex", memory);
   end
 
   assign data = memory[addr];
