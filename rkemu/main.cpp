b6443769477a83ef14f5dbe97f34b95f95f98530
kanade-k-1228 <kanade.k.1228@gmail.com>
2023-09-22 18:10:06 +0900

    wip

--- rkemu/main.cpp
+++ rkemu/main.cpp
@@ -10,31 +10,30 @@
 std::string esc_char(char);
 
 std::string man
-    = "rkemu [-o] [-t] [-s] [-a] [-d .dump] [-i .intr] .rk.bin\n"
-      "  -t:      set interval Time     命令実行の間で少し待ちます\n"
-      "  -s:      Step by step          命令を逐次実行します\n"
-      "  -o:      print Operatioin      命令を表示します\n"
-      "  -a:      dump All operation    全命令でダンプします\n"
-      "  -d FILE: Dump points file      ダンプ時刻ファイル\n"
-      "  -i FILE: Interrupt points file 割り込み時刻ファイル\n"
-      "     FILE: Emulation binary      エミュレートするバイナリファイル";
+    = "rkemu [-o] [-a] [-t NUM] [-d FILE.dl] [-e FILE.el] FILE.rk.bin\n"
+      "  -o:      print Operatioin     命令を表示\n"
+      "  -a:      dump All operation   全命令ダンプ\n"
+      "  -t NUM:  Terminal step        エミュレートの終了ステップ\n"
+      "  -d FILE: Dump list            ダンプ情報\n"
+      "  -e FILE: External event list  非同期外部イベント\n"
+      "     FILE: RISC-K binary file   バイナリファイル";
 
 int main(int argc, char* argv[]) {
   CPU cpu;
   DumpPoints dump_points;
   IntrPoints intr_points;
-  int terminal = -1;
-  bool print_opr = false;
-  bool dump_all = false;
+  int terminal_step = -1;  // opt arg t
+  bool print_opr = false;  // opt o
+  bool dump_all = false;   // opt a
 
   // Init Emulator with Comandline options
   opterr = 0;
-  for(int opt; (opt = getopt(argc, argv, "d:i:t:oa")) != -1;) {
-    if(opt == 'd') dump_points.init(optarg);
-    if(opt == 'i') intr_points.init(optarg);
+  for(int opt; (opt = getopt(argc, argv, "oat:d:e:")) != -1;) {
     if(opt == 'o') print_opr = true;
-    if(opt == 't') terminal = std::stoi(optarg);
     if(opt == 'a') dump_all = true;
+    if(opt == 't') terminal_step = std::stoi(optarg);
+    if(opt == 'd') dump_points.init(optarg);
+    if(opt == 'e') intr_points.init(optarg);
     if(opt == '?') {
       std::cout << man << std::endl;
       return EXIT_FAILURE;
@@ -51,7 +50,7 @@ int main(int argc, char* argv[]) {
             << "| Emulate: " << left(cpu.fname, 37) << "|" << std::endl;
   if(dump_points.use) std::cout << "|  - Dump: " << left(dump_points.fname, 37) << "|" << std::endl;
   if(dump_points.all) std::cout << "|  - Dump: All                                |" << std::endl;
-  if(intr_points.use) std::cout << "|  - Intr: " << left(intr_points.fname, 37) << "|" << std::endl;
+  if(intr_points.use) std::cout << "|  - Evnt: " << left(intr_points.fname, 37) << "|" << std::endl;
   std::cout << "+-----------------------------------------------+" << std::endl;
 
   // Run Emulator
@@ -81,7 +80,7 @@ int main(int argc, char* argv[]) {
     cpu.interrupt(intr_points.is_intr(t) ? intr_points.at(t).irq_flag : 0);
 
     // Exit Emulator
-    if(cpu.is_shutdowned() || (terminal != -1 && t > terminal)) break;
+    if(cpu.is_shutdowned() || (terminal_step != -1 && t > terminal_step)) break;
   }
   std::cout << std::endl
             << "=================================================" << std::endl;
